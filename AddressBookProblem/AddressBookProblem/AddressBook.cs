7e9744e3710b4c7229af0c439eddf08d7191dabb
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-04 13:37:11 +0530

    [Shivaraj] Implemented Adding Contact In Address Book

---
+++ AddressBookProblem/AddressBookProblem/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        //Create dictionary to store result
+        Dictionary<string, string> Contactlist = new Dictionary<string, string>();
+        public void AddContact()
+        {
+            Console.WriteLine("first Name:");
+            Contactlist.Add("first Name", Console.ReadLine());
+
+            Console.WriteLine("last Name:");
+            Contactlist.Add("last Name", Console.ReadLine());
+
+            Console.WriteLine("Address:");
+            Contactlist.Add("Address", Console.ReadLine());
+
+            Console.WriteLine("City:");
+            Contactlist.Add("City", Console.ReadLine());
+
+            Console.WriteLine("State:");
+            Contactlist.Add("State", Console.ReadLine());
+
+            Console.WriteLine("Zip:");
+            Contactlist.Add("Zip", Console.ReadLine());
+
+            Console.WriteLine("Phone number:");
+            Contactlist.Add("Phone number", Console.ReadLine());
+
+            Console.WriteLine("Email:");
+            Contactlist.Add("Email", Console.ReadLine());
+        }
+    }
+}
+
--- AddressBookProblem/AddressBookProblem/Program.cs
+++ AddressBookProblem/AddressBookProblem/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Address Book Problems");
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Problems");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
+        }
+    }
+}
+
+
+//UC-1
+//Ability to create a Contacts in Address
+//Book with first and last names, address,
+//city, state, zip, phone number and
+//email...
+//- Program is written using IDE like IntelliJ
+//- Every UC is in a separate Git Branch and then merged with main
+//- Naming Convention, Indentation, etc Code Hygiene will be checked during Review
+//- Git Check In Comments and Version History will be monitored
+
+
+//Welcome To Address Book Problems
+//first Name:
+//Shivaraj
+//last Name:
+//Gowda
+//Address:
+//Basaveshwarnagar
+//City:
+//Bangalore
+//State:
+//Karnataka
+//Zip:
+//560079
+//Phone number:
+//8618199771
+//Email:
+//4shivaraj.gowda @gmail.com
