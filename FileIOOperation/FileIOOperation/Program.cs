bf52206e47fc6621003eb5a1318f2d791b690f23
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-04 17:29:32 +0530

    [Shivaraj] Added File IO Implementation

---
+++ FileIOOperation/FileIOOperation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FileIOOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            JasonDataOperaton.JsonSerialize();
+            JasonDataOperaton.JsonDeserialize();
+            Console.ReadLine();
+        }
+        public static bool IsFileExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Not Exists");
+                return false;
+            }
+        }
+    }
+}
