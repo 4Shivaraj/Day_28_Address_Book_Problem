8769a4e918eafbd015b5c0446df475b764e19eb8
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-04 14:06:18 +0530

    [Shivaraj] Added Editing Option In Address Book

--- AddressBookProblem/AddressBookProblem/AddressBook.cs
+++ AddressBookProblem/AddressBookProblem/AddressBook.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class AddressBook
+    class AddressBookSystem
     {
-        //Create dictionary to store result
-        Dictionary<string, string> Contactlist = new Dictionary<string, string>();
+        //Generated dictionary for storing
+        Dictionary<string, string> Contactlist;
+        Dictionary<string, Dictionary<string, string>> AddressBook = new Dictionary<string, Dictionary<string, string>>();
         public void AddContact()
         {
-            Console.WriteLine("first Name:");
-            Contactlist.Add("first Name", Console.ReadLine());
+            //Adding contact to the list
+            Console.WriteLine("add contact");
+            Contactlist = new Dictionary<string, string>();
 
-            Console.WriteLine("last Name:");
-            Contactlist.Add("last Name", Console.ReadLine());
+            Console.WriteLine("First Name:");
+            Contactlist.Add("First Name", Console.ReadLine());
+
+            Console.WriteLine("Last Name:");
+            Contactlist.Add("Last Name", Console.ReadLine());
 
             Console.WriteLine("Address:");
             Contactlist.Add("Address", Console.ReadLine());
@@ -32,7 +37,85 @@ namespace AddressBookSystem
 
             Console.WriteLine("Email:");
             Contactlist.Add("Email", Console.ReadLine());
+            Contactlist.TryGetValue("First Name", out string FirstName);
+            Contactlist.TryGetValue("Last Name", out string LastName);
+            AddressBook.Add(FirstName + " " + LastName, Contactlist);
+            Console.WriteLine("contact added\n");
+        }
+        public void ViewContact()
+        {
+            //Check contact exist or not
+            Console.WriteLine("Enter full name:");
+            string ContactName = Console.ReadLine();
+            if (AddressBook.ContainsKey(ContactName))
+            {
+                Contactlist = new Dictionary<string, string>();
+                AddressBook.TryGetValue(ContactName, out Contactlist);
+                Console.WriteLine("First Name: " + Contactlist["first Name"]);
+
+                Console.WriteLine("Last Name:" + Contactlist["last Name"]);
+
+
+            }
+            else
+                Console.WriteLine("Contact doesn't exist");
+
+        }
+        public void EditContact()
+        {
+            //Edit information
+            Console.WriteLine("Enter full contact name");
+            String ContactName = Console.ReadLine();
+
+            if (AddressBook.ContainsKey(ContactName))
+            {
+                Console.WriteLine("enter choice");
+                Console.WriteLine("1. First Name    2. Last Name    3. Address ");
+                Console.WriteLine("4. City          5. State        6. Zip");
+                Console.WriteLine("7. Phone number  8. Email");
+
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter contact field:");
+                String Cotanctinfo = Console.ReadLine();
+
+                switch (Choice)
+                {
+                    case 1:
+                        AddressBook[ContactName]["First Name"] = Cotanctinfo;
+                        break;
+                    case 2:
+                        AddressBook[ContactName]["Last Name"] = Cotanctinfo;
+                        break;
+                    case 3:
+                        AddressBook[ContactName]["Address"] = Cotanctinfo;
+                        break;
+                    case 4:
+                        AddressBook[ContactName]["City"] = Cotanctinfo;
+                        break;
+                    case 5:
+                        AddressBook[ContactName]["State"] = Cotanctinfo;
+                        break;
+                    case 6:
+                        AddressBook[ContactName]["Zip"] = Cotanctinfo;
+                        break;
+                    case 7:
+                        AddressBook[ContactName]["Phone number"] = Cotanctinfo;
+                        break;
+                    case 8:
+                        AddressBook[ContactName]["Email"] = Cotanctinfo;
+                        break;
+
+                }
+            }
+            else
+                Console.WriteLine("contact doesn't exist");
+
         }
     }
 }
+
+
+
+
+
 
--- AddressBookProblem/AddressBookProblem/Program.cs
+++ AddressBookProblem/AddressBookProblem/Program.cs
@@ -6,17 +6,26 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook();
+
+            AddressBookSystem addressBook = new AddressBookSystem();
+
             while (true)
             {
                 Console.WriteLine("Welcome to Address Book Program");
-                Console.WriteLine("1. Create contact ");
+                Console.WriteLine("1. add contact   2. edit contact");
+                Console.WriteLine("3. view contact");
                 try
                 {
                     switch (Convert.ToInt32(Console.ReadLine()))
                     {
                         case 1:
                             addressBook.AddContact();
+                            break;
+                        case 2:
+                            addressBook.EditContact();
+                            break;
+                        case 3:
+                            addressBook.ViewContact();
                             break;
                     }
                 }
@@ -27,22 +36,25 @@ namespace AddressBookSystem
             }
         }
     }
+   
 }
 
 
-//UC-2
-//Ability to add a new Contact to Address Book - Use Console to add person details from AddressBookMain class
-//-Use Object Oriented Concepts to manage relationship between AddressBook and Contact Person
+//UC-3
+//Ability to edit existing contact person using their name
+//Use Console to edit person details
 
 //Welcome to Address Book Program
-//1. Create contact
+//1. add contact   2. edit contact
+//3. view contact
 //1
-//first Name:
+//add contact
+//First Name:
 //Shivaraj
-//last Name:
-//Gowda
+//Last Name:
+//K
 //Address:
-//Basaveshwarnagar
+//Basaveshwar Nagar
 //City:
 //Bangalore
 //State:
@@ -52,4 +64,41 @@ namespace AddressBookSystem
 //Phone number:
 //8618199771
 //Email:
-//4shivaraj.gowda @gmail.com
+//4shivayash @gmail.com
+//contact added
+
+//Welcome to Address Book Program
+//1. add contact   2. edit contact
+//3. view contact
+//2
+//Enter full contact name
+//Shivaraj K
+//enter choice
+//1. First Name    2. Last Name    3. Address
+//4. City          5. State        6. Zip
+//7. Phone number  8. Email
+//2
+//Enter contact field:
+//Gowda
+//Welcome to Address Book Program
+//1. add contact   2. edit contact
+//3. view contact
+//3
+//Enter full name:
+//Shivaraj K
+//wrong input
+//Welcome to Address Book Program
+//1. add contact   2. edit contact
+//3. view contact
+//3
+//Enter full name:
+//Shivaraj K
+//wrong input
+//Welcome to Address Book Program
+//1. add contact   2. edit contact
+//3. view contact
+//3
+//Enter full name:
+//Shivaraj K
+//First Name: Shivaraj
+//Last Name:Gowda
